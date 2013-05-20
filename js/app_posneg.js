d4d8a2ebfc6000d9dc26b3188ab7c8c643d39859
Alex Olieman <alex@olieman.net>
2013-05-20 02:20:27 +0200

    Basic functioning wordtree TODO: - tooltip formatting       - linking to wordtree

--- js/app_posneg.js
+++ js/app_posneg.js
@@ -4,50 +4,80 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     height = 300 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%Y-%m-%d %Hh").parse;
+var formatTime = d3.time.format("%d %b %Hh");
 
 var formatNumber = d3.format(",.0f");
 
 var x = d3.time.scale()
     .range([0, width]);
 
-var y = d3.scale.log()
+var yp = d3.scale.log()
     .range([height, 0]);
+	
+var yn = d3.scale.log()
+    .range([0, height]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
+var ypAxis = d3.svg.axis()
+    .scale(yp)
+    .orient("left")
+	.ticks(5,function(d, i) {
+		return formatNumber(d) - 1;
+	});
+	
+var ynAxis = d3.svg.axis()
+    .scale(yn)
     .orient("left")
 	.ticks(5,function(d, i) {
 		return formatNumber(d) - 1;
 	});
 
-var area = d3.svg.area()
+var area_pos = d3.svg.area()
     .x(function(d) { return x(d.hour); })
     .y0(height)
-    .y1(function(d) { return y(d.pos_count); });
+    .y1(function(d) { return yp(d.pos_count); });
+	
+var area_neg = d3.svg.area()
+    .x(function(d) { return x(d.hour); })
+    .y0(function(d) { return yn(d.neg_count); })
+    .y1(0);
 
 var svg = d3.select("#posnegtweetvis").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("height", 2 * height + margin.top + 2 * margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+var ttip = d3.select("#posnegtweetvis").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0)
+	.on("mouseout", function(d) {       
+		ttip.transition()        
+			.duration(500)      
+			.style("opacity", 0)
+			.each("end", function() {
+			  d3.select(this).style("top", "30px");
+			});
+	});
 
 d3.json("data/twitter/hourly_sums.json", function(data) {
   data.forEach(function(d) {
     d.hour = parseDate(d.hour);
 	d.pos_count += 1;
+	d.neg_count += 1;
   });
 
   x.domain(d3.extent(data, function(d) { return d.hour; }));
-  y.domain([1, d3.max(data, function(d) { return d.pos_count; })]);
+  yp.domain([1, d3.max(data, function(d) { return d.pos_count; })]);
+  yn.domain([1, d3.max(data, function(d) { return d.neg_count; })]);
 
   svg.append("path")
       .datum(data)
-      .attr("class", "area")
-      .attr("d", area);
+      .attr("class", "area posarea")
+      .attr("d", area_pos);
 
   svg.append("g")
       .attr("class", "x axis")
@@ -55,8 +85,8 @@ d3.json("data/twitter/hourly_sums.json", function(data) {
       .call(xAxis);
 
   svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
+      .attr("class", "y axis positive")
+      .call(ypAxis)
     .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
@@ -64,10 +94,61 @@ d3.json("data/twitter/hourly_sums.json", function(data) {
       .style("text-anchor", "end")
       .text("Positive count");
 	  
-  svg.selectAll('g.generator')
+  svg.append("g").selectAll('g.generator')
 	  .data(data)
 	  .enter().append("svg:circle")
-	  .attr("cx",function(d){ return x(d.hour);})
-	  .attr("cy", function(d){ return y(d.pos_count);})
-	  .attr("r", function(d){ return 2;});
+	  .attr("class", "positive")
+	  .attr("cx", function(d){ return x(d.hour);})
+	  .attr("cy", function(d){ return yp(d.pos_count);})
+	  // hier probeer ik de punten met count==0 te verbergen, maar dat lukt niet
+	  .attr("r", function(d){
+			if (d.pos_count) {return 2}
+			else {return 0};
+			})
+	  .on("mouseover", function(d) {      
+		ttip.transition()        
+			.duration(200)      
+			.style("opacity", .9);      
+		ttip.html(formatTime(d.hour) + "<br/>"  + d.pos_terms)
+			.style("background", "cornflowerblue")
+			.style("left", (d3.event.pageX) + "px")     
+			.style("top", (d3.event.pageY - 28) + "px");    
+		});
+
+	  
+  var neg = svg.append("g")
+	  .attr("class", "negative")
+	  .attr("transform", "translate(0," + (height + margin.bottom) + ")");
+	  
+	  neg.append("path")
+		.datum(data)
+		.attr("class", "area negarea")
+		.attr("d", area_neg);
+		
+	  neg.append("g")
+		.attr("class", "y axis negative")
+		.call(ynAxis)
+      .append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("x", -0.9*height)
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "start")
+		.text("Negative count");
+		
+	  neg.append("g").selectAll('g.generator')
+		.data(data)
+		.enter().append("svg:circle")
+		.attr("cx", function(d){ return x(d.hour);})
+		.attr("cy", function(d){ return yn(d.neg_count);})
+		.attr("r", function(d){ return 2;})
+		.on("mouseover", function(d) {      
+		ttip.transition()        
+			.duration(200)      
+			.style("opacity", .9);      
+		ttip.html(formatTime(d.hour) + "<br/>"  + d.neg_terms)
+			.style("background", "gold")
+			.style("left", (d3.event.pageX) + "px")     
+			.style("top", (d3.event.pageY - 28) + "px");    
+		});
 });
